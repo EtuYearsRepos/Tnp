5b24892f9ff14adc08fac6904a674868e5d30017
Armadias <follietmartin@gmail.com>
2021-04-12 15:26:28 +0200

    correction de bug pour js

--- src/main/resources/static/header.js
+++ src/main/resources/static/header.js
@@ -3,19 +3,19 @@ start();
 
 function start()
 {
-    var headerContent = '<h1>Voici un texte très long qui représentera le titre de ce site!</h1>';
+    var headerContent = 'Voici un texte très long qui représentera le titre de ce site!';
     
-    var title = document.createElement('div');
+    var title = document.createElement('h1');
     title.classList.add('test');
     title.dataset.itemId = '1';
     
     title.innerHTML = headerContent;
     
-    console.log(`Div ${header}`);
     
     var header = document.getElementsByClassName('header')[0];
     header.append(title);
-
+    
+    console.log(`Header ${header}`);
     console.log(`Titre ${title}`);
 
 }
