d8c5b0387e88344b2ecc74aee52629df73f8c1f5
Commandeur Nicolas <comcomnico@gmail.com>
2021-04-12 15:39:02 +0200

    Changed append to innerHTML and getElementByClassName by getElementById

--- src/main/resources/static/header.js
+++ src/main/resources/static/header.js
@@ -3,19 +3,8 @@ start();
 
 function start()
 {
-    var headerContent = 'Voici un texte très long qui représentera le titre de ce site!';
-    
-    var title = document.createElement('h1');
-    title.classList.add('test');
-    title.dataset.itemId = '1';
-    
-    title.innerHTML = headerContent;
-    
-    
-    var header = document.getElementsByClassName('header')[0];
-    header.append(title);
-    
-    console.log(`Header ${header}`);
-    console.log(`Titre ${title}`);
-
+    var headerContent = '<h1> Voici un texte très long qui représentera le titre de ce site! </h1>';
+    var header = document.getElementById('header');
+    // console.log(`Header ${header}`);
+    header.innerHTML = headerContent;
 }
