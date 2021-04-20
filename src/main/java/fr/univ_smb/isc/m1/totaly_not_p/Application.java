117b73b3c4d8c8c254fd579c9e35ff302daa451d
Armadias <follietmartin@gmail.com>
2021-04-20 17:51:08 +0200

    mistake

--- src/main/java/fr/univ_smb/isc/m1/totaly_not_p/Application.java
+++ src/main/java/fr/univ_smb/isc/m1/totaly_not_p/Application.java
@@ -2,11 +2,8 @@ package fr.univ_smb.isc.m1.totaly_not_p;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
-// @SpringBootApplication
-@SpringBootApplication(exclude = {XADataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
