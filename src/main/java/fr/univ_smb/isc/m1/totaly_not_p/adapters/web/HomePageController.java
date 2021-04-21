42cb50f40bca2b119054637d78c5e348d9c3ae38
Armadias <follietmartin@gmail.com>
2021-04-21 21:12:24 +0200

    ajout sélection pages

--- src/main/java/fr/univ_smb/isc/m1/totaly_not_p/adapters/web/HomePageController.java
+++ src/main/java/fr/univ_smb/isc/m1/totaly_not_p/adapters/web/HomePageController.java
@@ -1,14 +1,24 @@
 package fr.univ_smb.isc.m1.totaly_not_p.adapters.web;
 
 import fr.univ_smb.isc.m1.totaly_not_p.application.ComicsService;
+import fr.univ_smb.isc.m1.totaly_not_p.infrastructure.persistence.Comic;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomePageController {
+
+    private static final int ELEMENT_PER_PAGE = 10;
+    private static final int NUMBER_DISPLAY_PAGE = 4;
 
     private final ComicsService comicsService;
 
@@ -16,9 +26,25 @@ public class HomePageController {
         this.comicsService = comicsService;
     }
 
-    @GetMapping(value="/") // /?page=1  /?page=2 ...   --> comics from 0+range*(index-1) to range*index
-    public String index(Model model, @RequestParam(defaultValue = "1") int page) {
-        model.addAttribute("comics", comicsService.comicsPageRange(page-1, 10) );
+    @GetMapping(value="/")
+    public String homePage(Model model)
+    {
+        return findPage(model, 1);
+    }
+
+    @GetMapping(value="/{page}") // /?page=1  /?page=2 ...   --> comics from 0+range*(index-1) to range*index
+    public String findPage(Model model, @PathVariable(value = "page") int page) {
+
+        Page<Comic> p = comicsService.comicsPageRange(page - 1, ELEMENT_PER_PAGE);
+        List<Comic> listComics = p.getContent();
+
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", p.getTotalPages());
+        model.addAttribute("totalElements", p.getTotalElements());
+        model.addAttribute("numberDisplayPage", NUMBER_DISPLAY_PAGE);
+
+        model.addAttribute("comics", listComics );
+
         return "base";
     }
     /*comicsService.comicsFromTo(0+10*(page-1), 10*page)*/
